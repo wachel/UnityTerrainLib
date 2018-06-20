0a4c9f18e0211b08d90d6d0d4b79b4aa447d619a
hailongwang <hailongwang@tencent.com>
2018-06-20 13:32:59 +0800

    TerrainMix

---
+++ Assets/TerrainMix/Resources/Shaders/TerrainTextureIndex.shader
@@ -0,0 +1,67 @@
+﻿
+Shader "Mobile/TerrainTextureIndex" {
+	Properties {
+		_TexArray("Texture Array", 2DArray) = "" {}
+		_NormalArray("Normal Array", 2DArray) = "" {}
+		_IndexControl("Control Texture", 2D) = "black" {}
+		_HeightScale("HeightScale",float) = 2
+		_Normal("Normal",2D) = "bump"{}
+	}
+	SubShader {
+		Tags{ "RenderType" = "Opaque"  "Queue" = "Geometry+1"}
+		LOD 150
+ 
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows
+		#pragma target 3.0
+
+		sampler2D _IndexControl;
+		sampler2D _Normal;
+
+		struct Input {
+			float2 uv_IndexControl;
+			float2 uv_Normal;
+		};
+
+		UNITY_DECLARE_TEX2DARRAY(_TexArray);
+		UNITY_DECLARE_TEX2DARRAY(_NormalArray);
+		float4 _TerrainScaleOffset[32];
+		float4 _IndexControl_TexelSize;
+		float _HeightScale;
+ 
+		void surf(Input IN, inout SurfaceOutputStandard o) {
+			fixed4 alpha = tex2D(_IndexControl, IN.uv_IndexControl);
+			float2 pointUV = (floor(IN.uv_IndexControl * _IndexControl_TexelSize.zw) + 0.5) * _IndexControl_TexelSize.xy;
+			float4 index = tex2Dlod(_IndexControl, float4(pointUV, 0, 0));
+
+			float index0 = index.r * 255;
+			float2 uv0 = (IN.uv_IndexControl + _TerrainScaleOffset[index0].zw) * _TerrainScaleOffset[index0].xy;
+			fixed4 col0 = UNITY_SAMPLE_TEX2DARRAY(_TexArray, float3(uv0, index0));
+			fixed4 nor0 = UNITY_SAMPLE_TEX2DARRAY(_NormalArray, float3(uv0, index0));
+		
+			float index1 = index.g * 255; 
+			float2 uv1 = (IN.uv_IndexControl + _TerrainScaleOffset[index1].zw) * _TerrainScaleOffset[index1].xy;
+			fixed4 col1 = UNITY_SAMPLE_TEX2DARRAY(_TexArray, float3(uv1, index1));
+			fixed4 nor1 = UNITY_SAMPLE_TEX2DARRAY(_NormalArray, float3(uv1, index1));
+		
+			float height = 0;// (1 - abs(alpha.w * 2 - 1)) * _HeightScale;
+			float a = clamp(alpha.w + (-col0.a + col1.a) * height, 0, 1);
+			fixed4 col = lerp(col0, col1, a);
+			fixed4 nor = lerp(nor0, nor1, a);
+
+			fixed3 n = UnpackNormal(nor);//UnpackNormal(tex2D(_Normal, (IN.uv_Normal * 30)));// +_TerrainScaleOffset[0].zw) * _TerrainScaleOffset[0].xy));
+		
+			o.Albedo = col.rgb;
+			o.Normal = n;
+			o.Metallic = 0;
+			o.Smoothness = col.a;
+		}
+
+		ENDCG
+	}
+ 
+	Dependency "AddPassShader" = "Mobile/TerrainTextureAddPass"
+	Fallback "Mobile/VertexLit"
+}
+
+
---
+++ Assets/TerrainMix/TerrainTextureIndexUpdater.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TerrainTextureIndexUpdater : MonoBehaviour
+{
+    Terrain terrain;
+    TerrainData terrainData;
+
+    //[HideInInspector]
+    public Material bakeMat;
+    //[HideInInspector]
+    public Material renderMat;
+    //[HideInInspector]
+    public RenderTexture controlTexture;
+    //[HideInInspector]
+    public Texture textureArray;
+    public Texture normalTextureArray;
+
+    [Range(0,5)]
+    public float heightScale = 2;
+
+    RenderTexture rt0;
+    RenderTexture rt1;
+
+    private void OnEnable() {
+        terrain = GetComponent<Terrain>();
+        terrainData = terrain.terrainData;
+        bakeMat = new Material(Shader.Find("Hidden/BakeTextureIndex"));
+        rt0 = new RenderTexture(terrainData.alphamapWidth, terrainData.alphamapHeight, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear); 
+        rt1 = new RenderTexture(terrainData.alphamapWidth, terrainData.alphamapHeight, 24, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+
+        rt0.filterMode = FilterMode.Bilinear;
+        rt1.filterMode = FilterMode.Bilinear;
+
+        if (renderMat == null) {
+            renderMat = new Material(Shader.Find("Mobile/TerrainTextureIndex"));
+        } else {
+            renderMat.shader = Shader.Find("Mobile/TerrainTextureIndex");
+        }
+        terrain.materialType = Terrain.MaterialType.Custom;
+        terrain.materialTemplate = renderMat;
+
+        controlTexture = BakeControlTexture();
+        textureArray = CreateTextureArray();
+        normalTextureArray = CreateNormalTextureArray();
+        renderMat.SetTexture("_TexArray", textureArray);
+        renderMat.SetTexture("_NormalArray", normalTextureArray);
+        renderMat.SetFloat("_TexArrayNum", terrainData.splatPrototypes.Length);
+        UpdateMaterial();
+    }
+
+    private void OnDisable() {
+        terrain.materialType = Terrain.MaterialType.BuiltInStandard;
+    }
+
+    void Start () {
+		
+	}
+
+    void UpdateMaterial() {
+        renderMat.SetTexture("_IndexControl", BakeControlTexture());
+        renderMat.SetVectorArray("_TerrainScaleOffset", GetScaleOffsets(terrain.terrainData));
+        renderMat.SetFloat("_HeightScale", heightScale);
+        renderMat.SetTexture("_Normal", terrain.terrainData.splatPrototypes[0].normalMap);
+        //Shader.SetGlobalVectorArray("_TerrainScaleOffset", GetScaleOffsets(terrain.terrainData));
+    }
+	
+	void Update () {
+        if (!Application.isPlaying) {
+            UpdateMaterial();
+        }
+    }
+
+    RenderTexture BakeControlTexture() {
+        rt0.DiscardContents();
+        rt1.DiscardContents();
+        RenderTexture.active = rt1;
+        GL.Clear(true, true, new Color(0, 0, 0, 0));
+        RenderTexture.active = rt0;
+        GL.Clear(true, true, new Color(0, 0, 0, 0));
+        for (int i = 0; i < terrainData.alphamapTextures.Length; i++) {
+            RenderTexture.active = i % 2 == 0 ? rt1 : rt0;//pingpang
+            bakeMat.SetTexture("_LastResult", i % 2 == 0 ? rt0 : rt1);
+            bakeMat.SetTexture("_SplatAlpha", terrainData.alphamapTextures[i]);
+            bakeMat.SetFloat("_StartLayerIndex", i * 4);
+            Graphics.Blit(Texture2D.blackTexture, bakeMat);
+        }
+        GL.End();
+        RenderTexture result = RenderTexture.active;
+        RenderTexture.active = null;
+        return result;
+    }
+
+    Texture CreateTextureArray() {
+        Texture firstTexture = terrainData.splatPrototypes[0].texture;
+        RenderTexture rt = new RenderTexture(firstTexture.width, firstTexture.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        rt.wrapMode = TextureWrapMode.Repeat;
+        rt.useMipMap = true;
+        rt.filterMode = FilterMode.Bilinear;
+        rt.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray;
+        rt.volumeDepth = terrainData.splatPrototypes.Length;
+        rt.autoGenerateMips = true;
+
+        Material mat = new Material(Shader.Find("Hidden/BlitCopy"));
+        for (int i = 0; i < terrainData.splatPrototypes.Length; i++) {
+            Graphics.SetRenderTarget(rt, 0, CubemapFace.Unknown, i);
+            Graphics.Blit(terrainData.splatPrototypes[i].texture, mat);
+        }
+        return rt;
+    }
+
+    Texture2D CreateTexture(Color color) {
+        Texture2D tex = new Texture2D(1, 1);
+        tex.name = "ColorTexture";
+        tex.SetPixel(0, 0, color);
+        return tex;
+    }
+
+    Texture CreateNormalTextureArray() {
+        Texture firstTexture = terrainData.splatPrototypes[0].normalMap;
+        RenderTexture rt = new RenderTexture(firstTexture.width, firstTexture.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Linear);
+        rt.wrapMode = TextureWrapMode.Repeat;
+        rt.useMipMap = true;
+        rt.filterMode = FilterMode.Bilinear;
+        rt.dimension = UnityEngine.Rendering.TextureDimension.Tex2DArray;
+        rt.volumeDepth = terrainData.splatPrototypes.Length;
+        rt.autoGenerateMips = true;
+        Texture2D defaultNormal = CreateTexture(new Color(0.5f, 0.5f, 1));
+
+        Material mat = new Material(Shader.Find("Hidden/BlitCopy"));
+        for (int i = 0; i < terrainData.splatPrototypes.Length; i++) {
+            Graphics.SetRenderTarget(rt, 0, CubemapFace.Unknown, i);
+            if (terrainData.splatPrototypes[i].normalMap) {
+                Graphics.Blit(terrainData.splatPrototypes[i].normalMap, mat);
+            } else {
+                Graphics.Blit(defaultNormal, mat);
+            }
+        }
+        return rt;
+    }
+
+    Vector4[] GetScaleOffsets(TerrainData terrainData) {
+        Vector4[] scaleOffsets = new Vector4[terrainData.splatPrototypes.Length];
+        for (int i = 0; i < terrainData.splatPrototypes.Length; i++) {
+            Vector2 tileSize = terrainData.splatPrototypes[i].tileSize;
+            Vector2 scale = new Vector2(terrainData.size.x / tileSize.x, terrainData.size.z / tileSize.y);
+            Vector2 offset = terrainData.splatPrototypes[i].tileOffset;
+            scaleOffsets[i] = new Vector4(scale.x, scale.y, offset.x, offset.y);
+        }
+        return scaleOffsets;
+    }
+}
